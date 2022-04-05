2f7bd2fdbec700585a30b65096c0df4767154c16
murtaza <murtaza.derbendi@gmail.com>
2022-04-05 10:25:55 +0300

    initial commit

---
+++ src/main/java/com/example/simple_springboot_project/SimpleSpringbootProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.simple_springboot_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleSpringbootProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleSpringbootProjectApplication.class, args);
+    }
+
+}
