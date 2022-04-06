8346dd0547c2bf7e8869a701f1408495fdfd8e14
murtaza <murtaza.derbendi@gmail.com>
2022-04-06 15:04:38 +0300

    test commit

--- src/main/java/com/example/simple_springboot_project/SimpleSpringbootProjectApplication.java
+++ src/main/java/com/example/simple_springboot_project/SimpleSpringbootProjectApplication.java
@@ -8,6 +8,7 @@ public class SimpleSpringbootProjectApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SimpleSpringbootProjectApplication.class, args);
+        System.out.println("Hi");
     }
 
 }
